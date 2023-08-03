3449a0337d280f3021dbac14c85931c1770f0f9d
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-08-04 00:44:49 +0300

    api-service.js

---
+++ src/js/api-servise.js
@@ -0,0 +1,33 @@
+export default class PixabayApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchCards() {
+        console.log(this);
+        const API_KEY = '38626503-0c946b31d6d08b2c506c34012';
+        const BASE_URL = 'https://pixabay.com/api/';
+
+        return fetch(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
+        .then((response) => response.json())
+            .then(data => {
+                console.log(data)
+                this.page += 1
+                return data.hits;
+            })
+
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
