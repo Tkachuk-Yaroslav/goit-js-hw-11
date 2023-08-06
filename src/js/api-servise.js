9c0dd1d81042bbc333949c5b7966a5149f26e097
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-08-06 19:35:51 +0300

    api-servise.js
    закоментований синтаксис async await для апі

--- src/js/api-servise.js
+++ src/js/api-servise.js
@@ -6,7 +6,7 @@ export default class PixabayApiService {
         this.page = 1;
     }
 
-    fetchCards() {
+    async fetchCards() {
         console.log(this);
         const API_KEY = '38626503-0c946b31d6d08b2c506c34012';
         const BASE_URL = 'https://pixabay.com/api/';
@@ -25,7 +25,18 @@ export default class PixabayApiService {
         //         return data.hits;
         //     })
 
+
+
         //стало через аксіос
+
+
+
+        // const response = await axios.get(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`);
+        // this.page += 1;
+        // return response.data.hits;
+
+
+
         return axios.get(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
             .then(resp => {
             return resp.data;
